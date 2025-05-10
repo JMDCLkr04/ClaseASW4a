fb8e4e7f88682b17889b3a5edf3793a2e90637f8
JMDCLkr04 <majin325yt@gmail.com>
2025-05-10 11:11:45 -0500

    solución de condiguración de vite.config.ts

--- practica2/practica/vite.config.ts
+++ practica2/practica/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ClaseASW4a/', // <- Muy importante para GitHub Pages
+  base: '/master/', // <- Muy importante para GitHub Pages
   plugins: [tailwindcss()],
 })

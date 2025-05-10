1aded9363f215757b7fe5baf33fe0fafb69b733b
JMDCLkr04 <majin325yt@gmail.com>
2025-05-10 12:46:41 -0500

    intento de solucion de deploy2

--- practica2/practica/vite.config.ts
+++ practica2/practica/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/master/', // <- Muy importante para GitHub Pages
+  base: '/gh-pages/', // <- Muy importante para GitHub Pages
   plugins: [tailwindcss()],
 })
